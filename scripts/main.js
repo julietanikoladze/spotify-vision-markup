6bf5f3f63649f940c3eee43cabc24ff448cad93e
Julieta Nikoladze <julietanikoladze@gmail.com>
2026-03-27 00:45:44 +0400

    fix: search HTML layout and correct header/main sizes

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ document.getElementById("close-button").onclick = function () {
     const friendSection = document.getElementById("friends-section");
     mainSection.style.width = "calc(100vw - 25.9rem)";
     friendSection.style.display = "none";
-};
+}
 
 document.getElementById("open-friends").onclick = function () {
     const mainSection = document.getElementById("main-section");
